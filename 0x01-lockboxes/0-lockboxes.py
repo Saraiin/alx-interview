e53a59c0612956b1678ee6ff3218308ed6045623
saraiin <saraennaciri96@gmail.com>
2024-01-10 12:08:42 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,23 +4,17 @@ containing keys to other elements) are unlocked"""
 
 
 def canUnlockAll(boxes):
-    # Set to keep track of unlocked boxes
+    # first box is unlocked
     unlocked_boxes = {0}
 
-    # List to keep track of keys that can be used to unlock more boxes
+    # available keys in the firt box
     keys = boxes[0]
 
-    # Continue until there are no more keys
     while keys:
-        # Pop a key from the list
         key = keys.pop()
 
-        # Check if the key corresponds to a valid box and has not been unlocked yet
         if key not in unlocked_boxes and key < len(boxes):
-            # Add the newly unlocked box to the set of unlocked boxes
             unlocked_boxes.add(key)
-            # Extend the list of keys with the keys in the newly unlocked box
             keys.extend(boxes[key])
 
-    # Check if all boxes have been unlocked
     return len(unlocked_boxes) == len(boxes)
