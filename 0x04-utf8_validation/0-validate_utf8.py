761422a3440d9677dae46c57a1d53c9cd90cdda6
saraiin <saraennaciri96@gmail.com>
2024-02-01 21:53:40 +0000

    utf-8 function

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+def validUTF8(data):
+    def count_starting_bits(byte):
+        count = 0
+        while byte & (1 << (7 - count)):
+            count += 1
+        return count
+
+    i = 0
+    while i < len(data):
+        start_bits = count_starting_bits(data[i])
+
+        if start_bits == 0:
+            i += 1
+        elif start_bits == 1 or start_bits > 4:
+            return False
+        else:
+            for j in range(1, start_bits):
+                if i + j >= len(data) or (data[i + j] & 0b11000000) != 0b10000000:
+                    return False
+            i += start_bits
+
+    return True
